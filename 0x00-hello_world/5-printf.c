a30d48cfd5c4c66ca5d42f4854df97af416587f1
Eeid11 <eeid02388@gmail.com>
2023-10-05 19:36:22 -0700

    ee

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,17 +1,16 @@
 #include <stdo.h>
 
-/*
+/**
  * main - Entry point
  *
  * Description: A C programe file print using function printf.
  *
- * Always retern 0 (succsess)
+ * Return: Always retern 0 (succsess)
  *
- */
+*/
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-
 }
