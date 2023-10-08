8fb3da19e4c831a681c69c3f74f64a9bf9c804ea
Eeid11 <eeid02388@gmail.com>
2023-10-08 15:57:36 -0700

    eslam

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: print alphabet in lowercase
+ *
+ * Return: 0 (success)
+*/
+
+
+int main(void)
+{
+char x = 'a';
+while (ch != 'z')
+{
+putcher(ch);
+cd++;
+}
+putcher('\n');
+return (0);
+}
