2b3a5ac17d166866f2d18cd116ae5da10b9686f5
Eeid11 <eeid02388@gmail.com>
2023-10-08 07:59:06 -0700

    eslam

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,35 +8,17 @@
  * Description: positive or nigative
  *
  * Return: 0 (success)
-<<<<<<< HEAD
-=======
-*
->>>>>>> c7822e5035ccbb232c32aae7050b8138d37a6e6f
 */
 int main(void)
 {
 	int n;
-<<<<<<< HEAD
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (x == 0)
+	if (n == 0)
 		printf("%i is zero\n", n);
-	else if (x > 0)
+	else if (n > 0)
 		printf("%i is positive\n", n);
 	else
 		printf("%i is negative\n", n);
 	return (0);
 }
-=======
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (x == 0)
-		printf("%i is 0\n", n);
-	else if (x > 0)
-		printf("%i is positive\n", n);
-	else 
-		printf("\n", n);
-	return (0);
-}	
->>>>>>> c7822e5035ccbb232c32aae7050b8138d37a6e6f
