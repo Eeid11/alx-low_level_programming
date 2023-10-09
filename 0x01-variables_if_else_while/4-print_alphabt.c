7509e1897f6d96922099cbd2d96cb9f3ba7dc228
Eeid11 <eeid02388@gmail.com>
2023-10-09 04:01:29 -0700

    eslam

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print alphabet in lowercase
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+char x = 'a';
+while (x <= 'z')
+{
+if (x == 'e' || x == 'q')
+x++
+putchar(x);
+x++;
+}
+putchar('\n');
+return (0);
+}
