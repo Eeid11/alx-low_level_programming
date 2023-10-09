6978375f99c5f43e6685da45aa492491b5784574
Eeid11 <eeid02388@gmail.com>
2023-10-09 04:53:33 -0700

    eslam

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: print alphabet in lowercase
+ *
+ * Return: 0 (success)
+*/
+
+
+int main(void)
+{
+int x = 48;
+while (x <= 102)
+{
+putchar(x);
+if (x == 57)
+x += 39;
+x++;
+}
+putchar('\n');
+return (0);
+}
