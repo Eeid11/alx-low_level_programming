aeef55cc565a60b75735bf5944357def28718ec7
Eeid11 <eeid02388@gmail.com>
2023-10-09 06:59:40 -0700

    eslam

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+int x = 0, y;
+while (x <= 9)
+{
+y = 0;
+while (y <= 9)
+{
+if (x != Y && X < Y)
+{
+putchar(x + 48);
+putchar(Y + 48);
+if (x + y != 17)
+{
+putchar(',');
+putchar(' ');
+
+}
+}
+y++;
+}
+x++;
+}
+putchar('\n');
+return (0);
+}
