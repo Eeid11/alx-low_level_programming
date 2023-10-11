b1b1480518fa12cd16a49102358b45045fccd8cb
Eeid11 <eeid02388@gmail.com>
2023-10-11 07:24:56 -0700

    eslam

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_sign - utilizes on the _putchar function to print
+ *                      the alphabet a - z
+ *
+ * @n: take integer type input for function
+ *
+ * Return: 1 if +, 0 if 0, - if -
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (1);
+	}
+	else
+	{
+		 _putchar(45);
+		return (-1);
+	}
+}
