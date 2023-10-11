80174b029096b53146008de1010686bc668ed98c
Eeid11 <eeid02388@gmail.com>
2023-10-10 20:24:48 -0700

    eslam

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: prints _putchar by new line 
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
