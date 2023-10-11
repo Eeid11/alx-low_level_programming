533a0f5ed686fbdad14c0e2912582c30b46ac373
Eeid11 <eeid02388@gmail.com>
2023-10-11 05:27:31 -0700

    eslam

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,10 @@
 
 #include <stdio.h>
 /**
- * main - entry point
  *
- * Description: prints all single digit numbers of base 10 starting from 0
- *
- * Return: 0 (success)
+ * print_alphabet - utilaization on the _putchar function to print alpabet 
 */
 
-void print_alphabet(void);
-
-int main(void)
-{
-print_alphabet();
-return (0);
-}
 void print_alphabet(void)
 {
 char x = 'a';
