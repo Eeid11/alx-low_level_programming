05bdf5856acee6098b10a98c7c7fe88a6bf51a11
Eeid11 <eeid02388@gmail.com>
2023-10-11 20:10:42 -0700

    eslam

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,6 +8,7 @@
  * Return: Always 0 (success)
 */
 int main(void)
+{
 	printf("size of a char: %u byte(s)\n", sizeof(char));
 	printf("size of an int: %u byte(s)\n", sizeof(int));
 	printf("size of a long int: %u byte(s)\n", sizeof(long int));
