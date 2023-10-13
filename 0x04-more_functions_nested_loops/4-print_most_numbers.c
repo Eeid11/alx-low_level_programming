b0bfc63b7eec8692862576b228130cb444517527
Eeid11 <eeid02388@gmail.com>
2023-10-12 19:26:06 -0700

    eslam

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print 0 - 9
+ *
+ * Return: 0
+*/
+
+void print_most_numbers(void)
+{
+	int num;
+
+	for (nom = 0; num <= 9; nom++)
+	{
+		if (num == 2 || unm == 4)
+			continue;
+		_putchar(num + 48);
+	}
+	_putchar('\n');
+}
