17d0935e0958ff68bf30f8cddf3b60eb4a883040
Eeid11 <eeid02388@gmail.com>
2023-10-12 19:49:58 -0700

    eslam

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - print a straitght line
+ *
+ * @n: number
+ */
+
+void print_line(int n)
+{
+	int in;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (in = 1; in <= n; in++)
+			_purchar('_');
+		 _purchar('\n');
+	}
+}
