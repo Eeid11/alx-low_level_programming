549be243ac8f9db5354f26dad415a6ed7b6f2bce
Eeid11 <eeid02388@gmail.com>
2023-10-13 17:54:01 -0700

    eslam

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -3,24 +3,27 @@
 /**
  * print_diagonal - Draws a diagonal line on the terminal using the '\' character.
  * @n: The number of times the '\' character should be printed.
- * 
+ *
  * Return: void
- */
+*/
+
+
 void print_diagonal(int n)
 {
-    if (n <= 0)
-    {
-        _putchar('\n');
-        return;
-    }
+	int i, j;
 
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < i; j++)
-        {
-            _putchar(' ');
-        }
-        _putchar('\\');
-        _putchar('\n');
-    }
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+			_putchar(' ');
+		_putchar('\\');
+		_putchar('\n');
+
+	}
 }
