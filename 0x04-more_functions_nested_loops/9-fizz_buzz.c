cb3a1d22c4e05c0ea38c103de42b874b88cce9dd
Eeid11 <eeid02388@gmail.com>
2023-10-14 20:42:31 -0700

    eslam

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,22 +5,35 @@
  *
  * Return: Always 0
  */
+
 int main(void)
 {
-int i;
 
-for (i = 1; i <= 100; i++)
-{
-if (i % 3 == 0 && i % 5 == 0)
-printf("FizzBuzz ");
-else if (i % 3 == 0)
-printf("Fizz ");
-else if (i % 5 == 0)
-printf("Buzz ");
-else
-printf("%d ", i);
-}
 
-printf("\n");
-return 0;
+	int y = 0;
+
+	printf("%d\n", y++);
+	for (int i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && !(i % 5 == 0))
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0 && !(i % 3 == 0))
+		{
+			printf("Buzz");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		if (i < 100)
+		{
+			printf(" ");
+		}
+	}
 }
