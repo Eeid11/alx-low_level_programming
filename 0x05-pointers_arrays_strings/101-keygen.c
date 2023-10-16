9484d29f118ca3900d4799ca3ac4d3a520d5ae4a
Eeid11 <eeid02388@gmail.com>
2023-10-16 16:41:32 -0700

    eslam

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - inery point
+ *
+ * Return: zero
+*/
+
+int main(void)
+{
+	int sum;
+	char c;
+
+	strand(time(NULL));
+	while (sum <= 2645)
+	{
+		c = rand() % 128;
+		sum += c;
+		putchar(c);
+	}
+	putchar(2772 - sum);
+	return (0);
+}
