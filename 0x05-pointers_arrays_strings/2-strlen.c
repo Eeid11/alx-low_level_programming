67699ca82617a113c81f410b217eff4c9fe9c00f
Eeid11 <eeid02388@gmail.com>
2023-10-16 13:32:23 -0700

    eslam

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_strlen - returns the length of a string.
+ *
+ * @s: as input
+ *
+ *Return: *returns the length of a string
+*/
+
+int _strlen(char *s)
+{
+	int count;
+
+	for (count = 0; *s != '\o'; *s++)
+	{
+		count++;
+	}
+	return (count);
+}
