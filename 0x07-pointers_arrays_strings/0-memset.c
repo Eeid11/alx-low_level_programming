693a67117662cd41352bbd68bb39b28b62525dc9
Eslam eid <144378352+Eeid11@users.noreply.github.com>
2023-10-23 23:56:49 +0300

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,14 +9,20 @@
  *
  * Return: pointer
  */
+
+
 char *_memset(char *s, char b, unsigned int n)
 {
+	/**
+	 * declare an unsigned int
+	 * because we are storing a
+	 * value that will always be
+	 * non-negative (zero or positive)
+	*/
 	unsigned int i;
 
-	for (i  = 0; i < n; i++)
-	{
+	for (i = 0; i < n; i++)
 		s[i] = b;
-	}
+
 	return (s);
 }
-
