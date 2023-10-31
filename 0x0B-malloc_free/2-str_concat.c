76eed724671610b8b05ba0b4e58ea007132227ac
Eeid11 <eeid02388@gmail.com>
2023-10-31 04:54:11 -0700

    eslam

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -33,14 +33,14 @@ char *str_concat(char *s1, char *s2)
 	if (s1 == NULL)
 		s1 = '\0';
 	if (s2 == NULL)
-		S2 = '\0';
+		s2 = '\0';
 	size1 = _strlen(s1);
 	size2 = _strlen(s2);
 
 	m = malloc((size1 + size2) * sizeof(char) + 1);
 	if (m == 0)
 		return (0);
-	for (i = 0, i <= size1 + size2, i++)
+	for (i = 0; i <= size1 + size;, i++)
 	{
 		if (i < size1)
 			m[i] = s1[i];
