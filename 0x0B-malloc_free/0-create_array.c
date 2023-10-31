b2f716fb10628f87b46231da96c9b67b5848652e
Eeid11 <eeid02388@gmail.com>
2023-10-31 02:11:38 -0700

    esalm

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdlib.h>
 /**
  * create_array - function that creates an array of char
  *
@@ -15,7 +15,9 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0 || n == 0)
 		return (0);
+
 	while (size--)
 		n[size] = c;
+
 	return (n);
 }
