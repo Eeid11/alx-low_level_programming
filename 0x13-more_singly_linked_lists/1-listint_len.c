8d9b2d68c016d210d6d30050ce09e2f4b0d81a76
Eslam eid <eeid02388@gmail.com>
2023-11-20 19:06:20 -0800

    print lists

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint - the function return number of nude.
+ * @h: the pointer of head list
+ * Return: the number of nods.
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+    return count;
+}
