e40d52cc4974bdb416e1c3a11108711221b4d70d
Eslam eid <eeid02388@gmail.com>
2023-11-20 19:10:29 -0800

    print lists

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -8,13 +8,14 @@
 
 size_t listint_len(const listint_t *h)
 {
-	size_t count = 0;
+	size_t i = 0;
 
 	while (h != NULL)
 	{
-		count++;
+		
 		h = h->next;
+		i++;
 	}
 
-    return count;
+    return i;
 }
