b81d7089f1dcd607cef1b4a71e99d12f549d3350
Eslam eid <eeid02388@gmail.com>
2023-11-27 15:18:35 -0800

    read fil

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * read_textfile - the function read and print texefile
+ * @filename: pointer to file name
+ * @letters: number of lettres
+ * Return: number of letters
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t by;
+	char buf [1024];
+
+	if (!filename || !letters)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd = -1)
+		return (0);
+	by = read(fd, &buf[0], letters);
+	by = write(STDOUT_FILENO, &buf[0], letters);
+	close(fd);
+
+	return (by);
+}
