2eac18874ff26a430917c4ba330e9f73aa0616f6
Eslam eid <eeid02388@gmail.com>
2023-11-27 15:32:42 -0800

    eslam

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,12 +11,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	ssize_t by;
-	char buf [1024];
+	char buf[1024];
 
 	if (!filename || !letters)
 		return (0);
 	fd = open(filename, O_RDONLY);
-	if (fd = -1)
+	if (fd == -1)
 		return (0);
 	by = read(fd, &buf[0], letters);
 	by = write(STDOUT_FILENO, &buf[0], letters);
