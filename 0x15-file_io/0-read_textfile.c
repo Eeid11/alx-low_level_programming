d65775dbfb90d69487a26a41a5293577edd61038
Eslam eid <eeid02388@gmail.com>
2023-11-27 18:04:58 -0800

    error betty

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -19,7 +19,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fd == -1)
 		return (0);
 	by = read(fd, &buf[0], letters);
-	by = write(STDOUT_FILENO, &buf[0],by);
+	by = write(STDOUT_FILENO, &buf[0], by);
 	close(fd);
 
 	return (by);
